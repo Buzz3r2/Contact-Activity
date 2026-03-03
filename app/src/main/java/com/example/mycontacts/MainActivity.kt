46b7ffd7be9b43f7bf14a891e8a46eeb401676fd
Buzz3r2 <saadiqsteyn1@gmail.com>
2026-03-03 10:30:49 +0200

    Created a contacts app

---
+++ app/src/main/java/com/example/mycontacts/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.mycontacts
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val firstName = "Saadiq"
+        val lastName = "Steyn"
+        val address = "39A Ranelaugh Road"
+        val isMale = true
+        val dayOfBirth = "21"
+        val monthOfBirth = "October"
+        val yearOfBirth = "2006"
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
