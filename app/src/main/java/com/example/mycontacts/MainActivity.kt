ea1ee81c45e1ce5fe8af8d3616f889d66191612e
Buzz3r2 <saadiqsteyn1@gmail.com>
2026-03-09 15:51:16 +0200

    Added on to the MyContacts Code

--- app/src/main/java/com/example/mycontacts/MainActivity.kt
+++ app/src/main/java/com/example/mycontacts/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.mycontacts
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,13 +17,24 @@ class MainActivity : AppCompatActivity() {
         val dayOfBirth = "21"
         val monthOfBirth = "October"
         val yearOfBirth = "2006"
-        Log.d(
-            "Good evening",
-            firstName + " " + lastName + ", from what I recall, you live in " + myAddress +
-                    " and you were born on the " + dayOfBirth + " " + monthOfBirth + " " + yearOfBirth +
-                    " and it was also mentioned that you are " + (if (isMale) "male" else "female")
+        println("Good evening , $firstName, $lastName, from what I recall, you live at, $myAddress,"
+                "and you were born on the, $dayOfBirth, $monthOfBirth, $yearOfBirth," +
+                        "and it was also mentioned that you are, $isMale")
         )
+        val myAge = 19
+        val ageInHundredYears = 100 - myAge
+        val ageMonths = myAge + 12
+        val ageDays = myAge + 365
+        val remainder = myAge % 5
+
+        println("Current age: $myAge")
+        println("Years until 100: $ageInHundredYears")
+        println("Age in months: $ageMonths")
+        println("Age in days: $ageDays")
+        println("Remainder of when age is divided by 5: $remainder")
+
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
